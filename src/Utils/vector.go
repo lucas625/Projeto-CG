1e1170a076e867e01f796b946ca120a6e579a191
lucas625 <lagc@cin.ufpe.br>
2019-09-12 13:52:48 -0300

    starting ortogonalization

--- src/Utils/vector.go
+++ src/Utils/vector.go
@@ -40,22 +40,27 @@ func CMultVector(vect *Vector, k float64) Vector {
 	return vectAux
 }
 
-func SumVector(vect1 *Vector, vect2 *Vector) Vector {
+func SumVector(vect1 *Vector, vect2 *Vector, k1 float64, k2 float64) Vector {
 	/*
 		Function to sum 2 vectors.
 
 		Parameters:
 			vect1 - The first vector.
 			vect2 - The second vector.
+			k1    - Constant multiplying the first vector.
+			k2    - Constant multiplying the second vector.
 
 		Returns:
 			The resulting vector.
 	*/
 
+	vect1Aux := CMultVector(vect1, k1)
+	vect2Aux := CMultVector(vect2, k2)
+
 	vectAux := Vector{0,0,0}
-	vectAux.x = vect1.x + vect2.x
-	vectAux.y = vect1.y + vect2.y
-	vectAux.z = vect1.z + vect2.z
+	vectAux.x = vect1Aux.x + vect2Aux.x
+	vectAux.y = vect1Aux.y + vect2Aux.y
+	vectAux.z = vect1Aux.z + vect2Aux.z
 
 	return vectAux
 }
@@ -97,15 +102,49 @@ func ProjVector(vect1 *Vector, vect2 *Vector) Vector {
 
 }
 
+func OrtogonalizeVector(vect1 *Vector, vect2 *Vector) Vector {
+	/*
+		Function to check if two vectors are ortogonal to each other.
+
+		Parameters:
+			vect1 - The first vector.
+			vect2 - The second vector.
+
+		Returns:
+			The resulting vector.
+	*/
+	vectAux := ProjVector(vect1, vect2)
+	return SumVector(vect1, &vectAux, 1, -1)
+}
+
+func CheckOrtogonalVector(vect1 *Vector, vect2 *Vector) bool {
+	/*
+		Function to check if two vectors are ortogonal to each other.
+
+		Parameters:
+			vect1 - The first vector.
+			vect2 - The second vector.
+
+		Returns:
+			A boolean.
+	*/
+
+	if DotProduct(vect1, vect2) == 0 {
+		return true
+	}
+	return false
+
+}
+
 func main() {
-	a := Vector{10,20,30}
-	b := Vector{2,4,8}
+	a := Vector{1,0,0}
+	b := Vector{1,1,0}
 	c := CMultVector(&b, 3)
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
 
-	d := SumVector(&a, &b)
+	d := ProjVector(&a, &b)
 
 	fmt.Println(a, b, c, d)
 }

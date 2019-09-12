5d1983afe42586cc154410cf8db6b2955f0f1c1f
lucas625 <lagc@cin.ufpe.br>
2019-09-12 18:35:52 -0300

    fixing imports

--- teste.go
+++ teste.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/lucas625/Projeto-CG.git/src/utils"
 )
 
 func main() {
 	a := vector.Vector{1,2,3}
-	b := CMultVector(&a, 3)
+	b := vector.CMultVector(&a, 3)
 
 	fmt.Println(a, b)
 }

1d344810b9902df2d35b53cbf2864926a979dbe5
Lucas Aurelio Gomes Costa <lagc@ug5c17.windows.cin.ufpe.br>
2019-12-11 15:35:58 -0300

    ready to run

--- run/run.go
+++ run/run.go
@@ -1,35 +1,31 @@
 package main
 
 import (
-	"fmt"
-	// "github.com/lucas625/Projeto-CG/src/algorithms/pathtracing"
+
+	"github.com/lucas625/Projeto-CG/src/algorithms/pathtracing"
 	"github.com/lucas625/Projeto-CG/src/camera"
 	"github.com/lucas625/Projeto-CG/src/general"
 	"github.com/lucas625/Projeto-CG/src/light"
-	// "github.com/lucas625/Projeto-CG/src/screen"
-	// "github.com/lucas625/Projeto-CG/src/visualizer"
-	"github.com/lucas625/Projeto-CG/src/io/obj"
+	"github.com/lucas625/Projeto-CG/src/screen"
+	"github.com/lucas625/Projeto-CG/src/visualizer"
 )
 
 func main() {
-	// iterations := 5
-	// raysPerPixel := 1000
+	iterations := 5
+	raysPerPixel := 1000
 
 	cam := camera.LoadJSONCamera("resources/run/json/camera.json")
 	lights := light.LoadJSONLights("resources/run/json/light.json")
 	objects := general.LoadJSONObjects("resources/run/json/objects.json")
-	newObj := obj.ReadObj("resources/run/upperBox.obj")
-	objects.ObjList = append(objects.ObjList, *newObj)
 	outPath := "out/pathtracing"
-	fmt.Println(cam, lights, objects)
-	objects.WriteJSONObjects(outPath)
+
 	// getting screen
-	// camMatrix := camera.CamToWorld(cam)
-	// sc := screen.InitScreen(600, 600)
-	// sc.CamToWorld = &camMatrix
+	camMatrix := camera.CamToWorld(cam)
+	sc := screen.InitScreen(600, 600)
+	sc.CamToWorld = &camMatrix
 
-	// pathTracer := pathtracing.InitPathTracer(objects, &sc, cam, lights)
+	pathTracer := pathtracing.InitPathTracer(objects, &sc, cam, lights)
 
-	// colorScreen := pathTracer.Run(raysPerPixel, iterations)
-	// visualizer.WritePPM(*colorScreen, outPath)
+	colorScreen := pathTracer.Run(raysPerPixel, iterations)
+	visualizer.WritePPM(*colorScreen, outPath)
 }

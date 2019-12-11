024baf578a8c87de1b5322de8b708f80d61ad262
Lucas Aurelio Gomes Costa <lagc@ug5c17.windows.cin.ufpe.br>
2019-12-11 15:31:38 -0300

    fix2

--- run/run.go
+++ run/run.go
@@ -6,7 +6,7 @@ import (
 	"github.com/lucas625/Projeto-CG/src/camera"
 	"github.com/lucas625/Projeto-CG/src/general"
 	"github.com/lucas625/Projeto-CG/src/light"
-	"github.com/lucas625/Projeto-CG/src/screen"
+	// "github.com/lucas625/Projeto-CG/src/screen"
 	// "github.com/lucas625/Projeto-CG/src/visualizer"
 	"github.com/lucas625/Projeto-CG/src/io/obj"
 )
@@ -19,7 +19,7 @@ func main() {
 	lights := light.LoadJSONLights("resources/run/json/light.json")
 	objects := general.LoadJSONObjects("resources/run/json/objects.json")
 	newObj := obj.ReadObj("resources/run/upperBox.obj")
-	objects.ObjList = append(ObjList, newObj)
+	objects.ObjList = append(objects.ObjList, newObj)
 	outPath := "out/pathtracing"
 	fmt.Println(cam, lights, objects)
 	objects.WriteJSONObjects(outPath)
